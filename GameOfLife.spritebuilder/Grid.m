510e8397db01efddb04d08f31abe7ae8a5abf31f
Viktor Vaughn <sbsteven@andrew.cmu.edu>
2015-03-08 16:51:30 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -151,12 +151,12 @@ static const int GRID_COLUMNS = 10;
             Creature *currentCreature = _gridArray[i][j];
             if (currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4)
             {
-                currentCreature.isAlive = FALSE;
+                currentCreature.isAlive = NO;
                 numAlive--;
             }
             if (currentCreature.livingNeighbors == 3)
             {
-                currentCreature.isAlive = TRUE;
+                currentCreature.isAlive = YES;
                 numAlive++;
             }
         }
